1d505298dd62d48f3c088e5e56ce9aec3efc0a50
Daniel Otieno <danielotieno.ke@gmail.com>
2020-11-24 10:26:57 +0300

    chore:add event prevent

--- src/components/SellProForm.js
+++ src/components/SellProForm.js
@@ -34,6 +34,8 @@ function SellProgasModal(props) {
   });
 
   const handleAll = (e) => {
+    e.preventDefault();
+
     const form = e.currentTarget;
     if (form.checkValidity() === false) {
       e.preventDefault();
