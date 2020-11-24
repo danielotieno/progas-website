b67e25a7d7c4ca0f6356d16b8b098573ef9061a9
imahnama <muannama25@gmail.com>
2020-11-24 22:15:30 +0300

    Add SEO for site

--- src/pages/progasthankyou.js
+++ src/pages/progasthankyou.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 import Layout from '../components/Layout';
 import Bike from '../images/bike1.png';
+import SEO from '../components/SEO.js';
 
 export default function GetProgasThankYouPage() {
   return (
     <Layout>
+    <SEO
+        title='ProGas Thank You'
+        description='Thank you page. It shows successful filling of Get ProGas-form.'
+      />
       <Container className="get-form">
         <Row className="section-heading">
           <Col>
