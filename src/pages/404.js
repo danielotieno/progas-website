bd6c06d15280bf010675374aab3ededdb77675bc
Daniel Otieno <danielotieno.ke@gmail.com>
2020-11-25 03:40:56 +0300

    chore:fix path error

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,16 @@
-import React from "react"
-import Layout from "../components/layout"
-import { Link } from "gatsby"
+import React from 'react';
+import Layout from '../components/Layout';
+import { Link } from 'gatsby';
 
 const NotFound = () => {
   return (
     <Layout>
-      <h1 className="error-h1 text-center mt-5 pt-5">404: Page Not Found</h1>
-      <h3 className="text-center mt-5 mb-5">
-        <Link to="/index.js/">Go to HomePage</Link>
+      <h1 className='error-h1 text-center mt-5 pt-5'>404: Page Not Found</h1>
+      <h3 className='text-center mt-5 mb-5'>
+        <Link to='/index.js/'>Go to HomePage</Link>
       </h3>
     </Layout>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
